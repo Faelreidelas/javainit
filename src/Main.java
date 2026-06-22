bb221477e9781763975b04bd5a17bfd7b4b2c3a5
Faelreidelas <125436180+Faelreidelas@users.noreply.github.com>
2026-06-22 17:24:01 -0300

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[]args){
+      Scanner leia = new Scanner(System.in);
+      System.out.print("digite seu nome ");
+      String nome = leia.nextLine();
+      System.out.print("digite seu peso");
+      double peso = leia.nextDouble();
+      System.out.printf("O peso e  "+peso+" " );
+
+    }
+}
