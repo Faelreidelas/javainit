598570e49ddc8d7afa292983ca9d4e0d80f22279
FAEL <darimenezes7@gmail.com>
2026-06-23 16:29:53 -0300

    tst2

---
+++ src/atv2.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class atv2 {
+    static void main() {
+        Scanner leia = new Scanner(System.in);
+        System.out.print("digite um numero");
+        int num = leia.nextInt();
+
+
+
+    }
+}
---
+++ src/atv3.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class atv3 {
+    static void main() {
+        Scanner leia = new Scanner(System.in);
+        String continuar;
+
+        do{
+            System.out.println("eae pae");
+            System.out.print("quer continuar");
+            continuar = leia.nextLine();
+
+
+
+        }while( continuar.equalsIgnoreCase("s"));
+        System.out.println("eae pae");
+
+
+
+    }
+
+}
